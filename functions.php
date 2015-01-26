858cff793d3af68ec83c0dbd065e2723c6da28d6
David Oates <deoates@gmail.com>
2015-01-26 16:31:09 -0500

    dumbest shit ever

--- functions.php
+++ functions.php
@@ -314,13 +314,3 @@ function toolbox_enhanced_image_navigation( $url ) {
 add_filter( 'attachment_link', 'toolbox_enhanced_image_navigation' );
 
 
-function remove_menus () {
-global $menu;
-	$restricted = array(__('Dashboard'), __('Tools'), __('Users'), __('Settings'), __('Comments'));
-	end ($menu);
-	while (prev($menu)){
-		$value = explode(' ',$menu[key($menu)][0]);
-		if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){unset($menu[key($menu)]);}
-	}
-}
-add_action('admin_menu', 'remove_menus');
